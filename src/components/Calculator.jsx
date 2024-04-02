6acfc0094765668ba67276e729325ecdebc9cdc4
Shubham <shubhamgupta0720@gmail.com>
2024-04-02 08:18:00 +0530

    Components

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Calculator(){
+    return(
+        <h1>Calculator</h1>
+    )
+}
+
+export default Calculator;
---
+++ src/components/ThemeSwitcher.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ThemeSwitcher(){
+    return(
+        <h1>Switch</h1>
+    )
+}
+
+export default ThemeSwitcher;
